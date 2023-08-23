dfb0105bb4334aa57247bc334b4a6e8bbc3e2c23
Urooj Ahmad <urooz2002@gmail.com>
2023-08-23 08:02:47 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+//here i can use environment variable but i haven't use because i want to show you api url to show the transparency
+const API_URL = 'https://api.punkapi.com/v2/beers';
 
 function App() {
+  const [beers, setBeers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    const fetchBeers = async () => {
+      try {
+        let url = API_URL;
+
+        if (searchQuery) {
+          url += `?beer_name=${searchQuery}`;
+        }
+
+        const response = await fetch(url);
+        const data = await response.json();
+        setBeers(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchBeers();
+  }, [searchQuery]);
+
+  const handleSearch = event => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Punk API Beers</h1>
+        <input
+          type="text"
+          placeholder="Search by beer name"
+          value={searchQuery}
+          onChange={handleSearch}
+        />
       </header>
+      <div className="card-container">
+        {beers.map(beer => (
+          <div key={beer.id} className="beer-card">
+            <img src={beer.image_url} alt={beer.name} />
+            <h2>{beer.name}</h2>
+            <p>{beer.tagline}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
