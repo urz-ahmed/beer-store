dc9e505ac0cd53dbff1d134f68b662e3f875e40f
Ashfaque Mustaque <ashfaquemustaque423@gmail.com>
2023-10-30 23:40:50 +0530

    Health alert while searching is added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,63 +1,68 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
+const Home = () => {
+  const API_URL = 'https://api.punkapi.com/v2/beers';
 
-const Home = () =>{
-    const API_URL = 'https://api.punkapi.com/v2/beers';
+  const [beers, setBeers] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
-    const [beers, setBeers] = useState([]);
-    const [searchQuery, setSearchQuery] = useState('');
+  const showHealthAlert = () => {
+    toast.info('Beer is injurious to health');
+  };
 
-    useEffect(() => {
-        const fetchBeers = async () => {
-          try {
-            let url = API_URL;
-    
-            if (searchQuery) {
-              url += `?beer_name=${searchQuery}`;
-            }
-    
-            const response = await fetch(url);
-            const data = await response.json();
-            setBeers(data);
-          } catch (error) {
-            console.error(error);
-          }
-        };
-    
-        fetchBeers();
-      }, [searchQuery]);
+  useEffect(() => {
+    const fetchBeers = async () => {
+      try {
+        let url = API_URL;
 
+        if (searchQuery) {
+          url += `?beer_name=${searchQuery}`;
+        }
 
+        const response = await fetch(url);
+        const data = await response.json();
+        setBeers(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
-      const handleSearch = event => {
-        setSearchQuery(event.target.value);
-      };
-    return (
-        <>
-        <header className="App-header">
-          <h1>CraftsHop</h1>
-          <input
-            type="text"
-            placeholder="Discover Your Favorite Brew"
-            value={searchQuery}
-            onChange={handleSearch}
-          />
-        </header>
-        <div className="card-container">
-          {beers.map(beer => (
-            <div
-              key={beer.id}
-              className={`beer-card`}
-            >
-              <img src={beer.image_url} alt={beer.name} />
-              <h2>{beer.name}</h2>
-              <p>{beer.tagline}</p>
-            </div>
-          ))}
-        </div>
-        </>
-    )
+    fetchBeers();
+  }, [searchQuery]);
 
+  const handleSearch = event => {
+    setSearchQuery(event.target.value);
+  };
+
+  return (
+    <>
+      <header className="App-header">
+        <h1>CraftsHop</h1>
+        <input
+          type="text"
+          placeholder="Discover Your Favorite Brew"
+          value={searchQuery}
+          onChange={handleSearch}
+          onFocus={showHealthAlert}
+        />
+      </header>
+      <div className="card-container">
+        {beers.map(beer => (
+          <div
+            key={beer.id}
+            className={`beer-card`}
+          >
+            <img src={beer.image_url} alt={beer.name} />
+            <h2>{beer.name}</h2>
+            <p>{beer.tagline}</p>
+          </div>
+        ))}
+      </div>
+      <ToastContainer />
+    </>
+  )
 }
 
 export default Home;
